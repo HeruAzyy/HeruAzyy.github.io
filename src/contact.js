9e020098d4bfe98b75b957cc623a04ae18910fba
Heru Azy <heruazyp@gmail.com>
2023-11-09 22:07:18 +0700

    Fix - Perbaikan dan cofigurasi plugin prettier tailwinds

--- src/contact.js
+++ src/contact.js
@@ -1,22 +1,22 @@
 const scriptURL =
-  'https://script.google.com/macros/s/AKfycbyYWbXlZxdrwaKBjqkmSIChqfKDI9dz0rNbqohkeUsVyvsZThwrfsr_wYNw5p3ZChtxOw/exec'
-const form = document.forms['heru-azy-contact-form']
-const btnKirim = document.querySelector('.btn-kirim')
-const btnLoading = document.querySelector('.btn-loading')
-const textAlert = document.querySelector('.text-alert')
+  "https://script.google.com/macros/s/AKfycbyYWbXlZxdrwaKBjqkmSIChqfKDI9dz0rNbqohkeUsVyvsZThwrfsr_wYNw5p3ZChtxOw/exec";
+const form = document.forms["heru-azy-contact-form"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const textAlert = document.querySelector(".text-alert");
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-  btnLoading.classList.toggle('hidden')
-  btnKirim.classList.toggle('hidden')
-  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+  btnLoading.classList.toggle("hidden");
+  btnKirim.classList.toggle("hidden");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
     .then((response) => {
-      btnLoading.classList.toggle('hidden')
-      btnKirim.classList.toggle('hidden')
-      alert('Terimakasih. Pesan anda sudah saya terima✨')
-      form.reset()
-      console.log('Success!', response)
+      btnLoading.classList.toggle("hidden");
+      btnKirim.classList.toggle("hidden");
+      alert("Terimakasih. Pesan anda sudah saya terima✨");
+      form.reset();
+      console.log("Success!", response);
     })
-    .catch((error) => console.error('Error!', error.message))
-})
+    .catch((error) => console.error("Error!", error.message));
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['*.html'],
+  content: ["*.html"],
   theme: {
     container: {
-      center: true
+      center: true,
     },
     fontFamily: {
-      roboto: 'Roboto'
+      roboto: "Roboto",
     },
     animationDelay: {
-      2500: '2500ms',
-      2700: '2700ms',
-      2900: '2900ms',
-      3100: '3100ms',
-      3300: '3300ms',
-      3500: '3500ms'
+      2500: "2500ms",
+      2700: "2700ms",
+      2900: "2900ms",
+      3100: "3100ms",
+      3300: "3300ms",
+      3500: "3500ms",
     },
     extend: {
       colors: {
-        dark: '#14213d',
-        light: '#E5E5E5',
-        yellow: '#FCA311'
-      }
-    }
+        dark: "#14213d",
+        light: "#E5E5E5",
+        yellow: "#FCA311",
+      },
+    },
   },
-  plugins: [require('tailwindcss-animation-delay')]
-}
+  plugins: [require("tailwindcss-animation-delay")],
+};

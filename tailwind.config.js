526da296681c09b864a81c3002b29e00dcadd8a9
Heru Azy <heruazyp@gmail.com>
2023-12-09 17:32:37 +0700

    Fix: delete prettier tailwind plugin, format to tab size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html"],
-  theme: {
-    container: {
-      center: true,
-    },
-    fontFamily: {
-      roboto: "Roboto",
-    },
-    extend: {
-      animationDelay: {
-        1700: "1700ms",
-        2500: "2500ms",
-      },
-      colors: {
-        dark: "#14213d",
-        light: "#E5E5E5",
-        yellow: "#FCA311",
-      },
-    },
-  },
-  plugins: [require("tailwindcss-animation-delay")],
+	content: ['*.html'],
+	theme: {
+		container: {
+			center: true
+		},
+		fontFamily: {
+			roboto: 'Roboto'
+		},
+		extend: {
+			animationDelay: {
+				1700: '1700ms',
+				2500: '2500ms'
+			},
+			colors: {
+				dark: '#14213d',
+				light: '#E5E5E5',
+				yellow: '#FCA311'
+			}
+		}
+	},
+	plugins: [require('tailwindcss-animation-delay')]
 };
